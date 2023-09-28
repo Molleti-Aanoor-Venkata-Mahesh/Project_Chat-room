48915d00477a536480321ac63dfa1d66ee7224c4
jagadeesh-atla <vashishtapelapudi@gmail.com>
2023-09-28 17:03:14 +0530

    initial commit

---
+++ client/src/utils/apirouts.js
@@ -0,0 +1,7 @@
+export const host = import.meta.env.VITE_HOST_URL;
+export const regRoute = `${host}/api/auth/reg`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
